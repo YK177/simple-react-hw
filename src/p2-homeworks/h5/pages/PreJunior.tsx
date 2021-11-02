82b4b06e5021ee0ecba50851c237400140249f7b
YK177 <kotenev.yuri@gmail.com>
2021-11-02 09:10:59 +0400

    Some issues fixed

--- src/p2-homeworks/h5/pages/PreJunior.tsx
+++ src/p2-homeworks/h5/pages/PreJunior.tsx
@@ -8,7 +8,7 @@ import HW4 from '../../h4/HW4'
 function PreJunior() {
     return (
         <div className={s.container}>
-            {/*<HW1/>*/}
+            <HW1/>
             <HW2/>
             <HW3/>
             <HW4/>
