70cb877cacbee042bf33c0aadbe2e258a3539c69
YK177 <kotenev.yuri@gmail.com>
2021-11-04 16:39:56 +0400

    HW6

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 import {restoreState, saveState} from './localStorage/localStorage'
+import s from './HW6.module.css'
 
 function HW6() {
     const [value, setValue] = useState<string>('')
@@ -10,29 +11,33 @@ function HW6() {
         saveState<string>('editable-span-value', value)
     }
     const restore = () => {
-        // setValue()
+        setValue(restoreState<string>('editable-span-value', value))
     }
 
     return (
-        <div>
+        <div className={'task'}>
             <hr/>
             homeworks 6
 
             {/*should work (должно работать)*/}
-            <div>
-                <SuperEditableSpan
-                    value={value}
-                    onChangeText={setValue}
-                    spanProps={{children: value ? undefined : 'enter text...'}}
-                />
-            </div>
-            <SuperButton onClick={save}>save</SuperButton>
-            <SuperButton onClick={restore}>restore</SuperButton>
+            <div className={s.hw6}>
+                <div className={s.item}>
+                    <SuperEditableSpan
+                        value={value}
+                        onChangeText={setValue}
+                        spanProps={{children: value ? undefined : 'enter text...'}}
+                    />
+                </div>
+                <div className={s.buttons}>
+                    <SuperButton className={`${s.btn} ${s.green}`} onClick={save}>save</SuperButton>
+                    <SuperButton className={`${s.btn} ${s.blue}`} onClick={restore}>restore</SuperButton>
+                </div>
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperEditableSpan/>*/}
-            <hr/>
+                <hr/>
+                {/*для личного творчества, могу проверить*/}
+                {/*<AlternativeSuperEditableSpan/>*/}
+                <hr/>
+            </div>
         </div>
     )
 }
--- src/p2-homeworks/h6/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/p2-homeworks/h6/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -1,5 +1,6 @@
 import React, {DetailedHTMLProps, InputHTMLAttributes, HTMLAttributes, useState} from 'react'
 import SuperInputText from '../../../h4/common/c1-SuperInputText/SuperInputText'
+import s from './SuperEditableSpan.module.css'
 
 // тип пропсов обычного инпута
 type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
@@ -31,22 +32,26 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
     const {children, onDoubleClick, className, ...restSpanProps} = spanProps || {}
 
     const onEnterCallback = () => {
-        // setEditMode() // выключить editMode при нажатии Enter
+        setEditMode(false) // выключить editMode при нажатии Enter
 
         onEnter && onEnter()
     }
     const onBlurCallback = (e: React.FocusEvent<HTMLInputElement>) => {
-        // setEditMode() // выключить editMode при нажатии за пределами инпута
+        setEditMode(false) // выключить editMode при нажатии за пределами инпута
 
         onBlur && onBlur(e)
     }
     const onDoubleClickCallBack = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
-        // setEditMode() // включить editMode при двойном клике
+        setEditMode(true) // включить editMode при двойном клике
 
         onDoubleClick && onDoubleClick(e)
     }
 
-    const spanClassName = `${'сделать красивый стиль для спана'} ${className}`
+    const onMouseEnterHandler=()=>{
+
+    }
+
+    const spanClassName = `${s.default} ${className}`
 
     return (
         <>
@@ -63,11 +68,18 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
                     <span
                         onDoubleClick={onDoubleClickCallBack}
                         className={spanClassName}
+                        onMouseEnter={onMouseEnterHandler}
 
                         {...restSpanProps}
                     >
                         {/*если нет захардкодженного текста для спана, то значение инпута*/}
                         {children || restProps.value}
+                        <svg
+                            version="1.1" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                            viewBox="0 0 32 32">
+                            <path fill="#9acd32"
+                                  d="M27 0c2.761 0 5 2.239 5 5 0 1.126-0.372 2.164-1 3l-2 2-7-7 2-2c0.836-0.628 1.874-1 3-1zM2 23l-2 9 9-2 18.5-18.5-7-7-18.5 18.5zM22.362 11.362l-14 14-1.724-1.724 14-14 1.724 1.724z"/>
+                        </svg>
                     </span>
                 )
             }
